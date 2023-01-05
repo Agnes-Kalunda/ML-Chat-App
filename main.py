ab73939b334db0153870c17b9edcfd5ca3b64c41
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-05 15:43:20 +0300

    trained model to choose best match for response

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-my_bot = ChatBot(name='PyBot', read_only =True)
+my_bot = ChatBot(name='PyBot', read_only =True,
+                logic_adapters= 
+                ['chatterbot.logic.MathematicalEvaluation',
+                'chatterbot.logic.BestMatch'])
 
 small_talk = ['Hello!',
                 'Hi', 'How are you today?', 'Sorry to hear that', 'Glad to hear that!']
