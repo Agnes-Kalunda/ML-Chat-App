5625026544d49f0239e4a0e9bfcdf15132f4d273
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-05 15:23:34 +0300

    added bot responses

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 my_bot = ChatBot(name='PyBot', read_only =True)
+
+small_talk = ['Hello!',
+                'Hi', 'How are you today?', 'Sorry to hear that', 'Glad to hear that!']
